a73e82a6dc843f7a33593befcd6871d3bcf7ae2f
Rvux86-64 <hamza_siddiqui2023@hotmail.com>
2025-12-09 11:50:24 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 sudo su
 apt update
 apt install build-essential gcc g++ mtools qemu-system-x86 make 
-git clone https://github.com/Rvux86-64/ubuntu_dev.git
-cd ./ubuntu_dev
 ./run.sh
 git add .
 git commit -m "ran program"
